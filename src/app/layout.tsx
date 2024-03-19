c821a718a9c6f4c4d926f2d1d377ffa9e78964c8
Ankit Yadav <mahesh9426346889@gmail.com>
2024-03-19 17:05:41 +0530

    Meta data changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
           href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"
           crossOrigin="anonymous"
         />
-        <title>WebPage Builder</title>
+        <title>EZcompose</title>
       </head>
 
       <body className={inter.className}>
